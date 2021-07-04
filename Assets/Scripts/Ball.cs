e9c986baee7568d2e27877b86e1f1b423d0e0b05
Tyler J. Johnston <tylerj.johnston@outlook.com>
2021-07-04 16:37:20 -0500

    56. Launch the Ball
    Added functionality to launch the ball from paddle on left mouseclick if the ball was locked to the paddle. Added serialized xPush and yPush parameters to the Ball script. Added a hasLaunched state value in the Ball script. Added LaunchOnMouseClick() to Ball script, which handles the logic for the launching.

--- Assets/Scripts/Ball.cs
+++ Assets/Scripts/Ball.cs
@@ -1,3 +1,4 @@
+using System;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
@@ -6,9 +7,12 @@ public class Ball : MonoBehaviour
 {
     // config parameters
     [SerializeField] Paddle paddle1;
+    [SerializeField] float xPush = 2f;
+    [SerializeField] float yPush = 10f;
 
     // state
     Vector2 paddleToBallVector;
+    Boolean hasLaunched = false;
 
     // Start is called before the first frame update
     void Start()
@@ -19,7 +23,31 @@ public class Ball : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
+        if (!hasLaunched)
+        {
+            // Keep Ball Glued to Paddle
+            LockToPaddle();
+            // On MouseClick, Launch Ball. 
+            LaunchOnMouseClick();
+        }
+    }
+
+    private void LaunchOnMouseClick()
+    {
+        // On left-click
+        if (Input.GetMouseButtonDown(0))
+        {
+            hasLaunched = true;
+            GetComponent<Rigidbody2D>().velocity = new Vector2(xPush, yPush);
+            
+        }
+    }
+
+    private void LockToPaddle()
+    {
+        // Get Paddle Position. 
         Vector2 paddlePos = new Vector2(paddle1.transform.position.x, paddle1.transform.position.y);
+        // Update Ball Position relative to Paddle Position. 
         transform.position = paddlePos + paddleToBallVector;
     }
 }
