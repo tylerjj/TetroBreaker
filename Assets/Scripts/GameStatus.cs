38e8cfbeb903ab58368ed0e846db75f9808c4802
Tyler J. Johnston <tylerj.johnston@outlook.com>
2021-07-16 11:42:18 -0500

    67. Using Time.timeScale
    Created a Game Status object/prefab which we can use to alter the game speed of each level. Game Status prefab added to Level 1 and Level 2.

---
+++ Assets/Scripts/GameStatus.cs
@@ -0,0 +1,20 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GameStatus : MonoBehaviour
+{
+    [Range(.1f, 10f)][SerializeField] float gameSpeed = 1f;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        Time.timeScale = gameSpeed;
+    }
+}
