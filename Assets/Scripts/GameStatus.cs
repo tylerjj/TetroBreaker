b1c29678763145ce94828395217ad33606a6626e
Tyler J. Johnston <tylerj.johnston@outlook.com>
2021-07-16 23:15:29 -0500

    69. Display Player Score
    Removed SuccessButton from GameCanvas.
    Added Score Text to GameCanvas.
    Added font for Score Text and adjusted it to my liking.
    Wrote logic in GameStatus.cs to update the Score Text every time the score is changed.

--- Assets/Scripts/GameStatus.cs
+++ Assets/Scripts/GameStatus.cs
@@ -1,15 +1,22 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using TMPro;
 
 public class GameStatus : MonoBehaviour
 {
     // config params
     [Range(.1f, 10f)][SerializeField] float gameSpeed = 1f;
     [SerializeField] int pointsPerBreakableObjectDamaged = 83;
+    [SerializeField] TextMeshProUGUI scoreText;
 
     // state variables
     [SerializeField] int currentScore = 0; //Serialized for debugging purposes.
+
+    private void Start()
+    {
+        scoreText.text = currentScore.ToString();
+    }
 
     // Update is called once per frame
     void Update()
@@ -20,5 +27,6 @@ public class GameStatus : MonoBehaviour
     public void AddToScore()
     {
         currentScore += pointsPerBreakableObjectDamaged;
+        scoreText.text = currentScore.ToString();
     }
 }
